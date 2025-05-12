4fc0dafeef5d3f854282375fc74a8d0f6ee12a4e
Your Name <you@example.com>
2025-05-12 13:53:28 +0000

    debug statements

--- frontend/src/components/CustomEdge.jsx
+++ frontend/src/components/CustomEdge.jsx
@@ -52,6 +52,7 @@ export default function CustomEdge({
   const [isActive, setIsActive] = useState(false);
   
   // Customize path based on handle positions
+  const isSameSide = false;
 	//const isSameSide = sourcePosition === targetPosition;
   
   // Calculate path based on handle positions

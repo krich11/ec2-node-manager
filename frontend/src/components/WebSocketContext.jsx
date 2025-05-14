4a9500fa9a0e898f96cf1a3579586d640b9659cd
Your Name <you@example.com>
2025-05-14 22:14:57 +0000

    frontend ws action to backend

--- frontend/src/components/WebSocketContext.jsx
+++ frontend/src/components/WebSocketContext.jsx
@@ -50,7 +50,7 @@ const wsRef = useRef(null);
           //setEdges((eds) => eds.filter((e) => e.id !== msg.edgeId));
           break;
         default:
-          console.log("Unhandled websocket message: ${msg.type}");
+          console.log(f"Unhandled websocket message: ${msg.type}");
       }
 
     };
